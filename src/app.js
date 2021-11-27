acfe56f3a9b8166fccb30ce523fd9489b85ae44b
Beshil <ersanbievbesh@gmail.com>
2021-11-27 11:18:20 +0300

    start work

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+import { BackgroundModule } from './modules/background.module'
 import './styles.css'
-
+const backColor = new BackgroundModule('background', 'Фон документа')
+backColor.toHTML()
  
  
  
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,34 @@
-import {Module} from '../core/module'
-
+import {Module} from '../core/module' 
 export class BackgroundModule extends Module {
+  constructor(type, text){
+    super(type, text)
+    this.type = type
+    this.text = text
+  }
+  toHTML(){
+    const menu = document.querySelector('#menu')
+    console.log(menu);
+  }
+   
+}
 
+
+const colors = ['#c77c7c', '#003311', '#990022', '#e31o54', '#18D7FF', '#34FF18']
+
+
+
+function setColor(element) {
+	const color = getRandomColor()
+	element.style.backgroundColor = color
+	element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+	element.style.backgroundColor = '#1e11d1'
+	element.style.boxShadow = '0 0 2px #1e11d1'
+}
+
+function getRandomColor() {
+	const index = Math.floor(Math.random() * colors.length)
+	return colors[index]
 }
