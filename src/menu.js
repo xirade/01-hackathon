502a138e1811515ac92ed044b67e1e284c502428
xirade <6myusic6@gmail.com>
2021-11-27 20:37:47 +0200

    fixed bug with children

--- src/menu.js
+++ src/menu.js
@@ -10,7 +10,7 @@ class ContextMenu extends Menu {
     this.el.style.top = `${cursorY}px`;
     this.el.style.left = `${cursorX}px`;
 
-    this.el.children.forEach((element, index) => {
+    [...this.el.children].forEach((element, index) => {
       this.el.children[index].style.transform = "rotateY(0deg) rotateX(0deg)";
       this.el.children[index].style.webkitTransform =
         " rotateY(0deg) rotateX(0deg)";
@@ -21,7 +21,7 @@ class ContextMenu extends Menu {
     this.el.classList.add("open");
   }
   close() {
-    this.el.children.forEach((element, index) => {
+    [...this.el.children].forEach((element, index) => {
       this.el.children[index].style.transform = "rotateY(90deg) rotateX(45deg)";
       this.el.children[index].style.webkitTransform =
         "rotateY(90deg) rotateX(45deg)";
